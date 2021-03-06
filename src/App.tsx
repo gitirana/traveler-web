9d6f2c012ce41545dbdff524fe7e9b91093fc16a
gitirana <tlgc@poli.br>
2021-03-05 21:29:28 -0300

    header component and input focus

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import usePersistedState from './utils/usePersistedState';
 import GlobalStyle from './styles/global';
 import light from './styles/themes/light';
 import Card from './components/Card/Card';
+import Header from './components/Header/Header';
 
 function App() {
   const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', light);
@@ -12,11 +13,11 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Header />
       <div className="App">
         <h1>Hello, Travellers!</h1>
         <p>Hello, Travellers!</p>
       </div>
-
       <Card />
     </ThemeProvider>
   );
--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -1,20 +1,20 @@
 import 'styled-components';
 
 declare module 'styled-components' {
-    export interface DefaultTheme {
-        title: string;
-        colors: {
-            blue: string,
-            orange: string,
-            title: string,
-            text: string,
-            subText: string,
-            primaryShape: string,
-            secundaryShape: string,
-            red: string,
-            green: string,
-            lightBlue: string,
-            background: string
-        }
-    }
+  export interface DefaultTheme {
+    title: string;
+    colors: {
+      blue: string;
+      orange: string;
+      title: string;
+      text: string;
+      subText: string;
+      primaryShape: string;
+      secundaryShape: string;
+      red: string;
+      green: string;
+      lightBlue: string;
+      background: string;
+    };
+  }
 }
